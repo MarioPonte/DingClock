d78423654a5b39122a592d1a12c0d126503ea82c
Mário Ponte <ponteolavo30@gmail.com>
2023-11-16 18:27:44 +0000

    adding precision for chess clock

--- App.js
+++ App.js
@@ -3,8 +3,9 @@ import { MainView, TextOptionsButton, StartButton, PlayerButton, TextBtn, TextPl
 import { TouchableOpacity } from 'react-native';
 
 const ChessClock = () => {
-  const [player1Time, setPlayer1Time] = useState(300); // Initial Time
-  const [player2Time, setPlayer2Time] = useState(300);
+  const initialTimeInSeconds = 60;
+  const [player1Time, setPlayer1Time] = useState(initialTimeInSeconds * 1000); // Initial Time in milliseconds
+  const [player2Time, setPlayer2Time] = useState(initialTimeInSeconds * 1000);
   const [activePlayer, setActivePlayer] = useState(0); // 0 for nobody, 1 for player 1, 2 for player 2
 
   useEffect(() => {
@@ -12,9 +13,9 @@ const ChessClock = () => {
 
     if (activePlayer !== 0 && player1Time > 0 && player2Time > 0) {
       interval = setInterval(() => {
-        if (activePlayer === 1) setPlayer1Time((prevTime) => prevTime - 1);
-        else setPlayer2Time((prevTime) => prevTime - 1);
-      }, 1000);
+        if (activePlayer === 1) setPlayer1Time((prevTime) => prevTime - 100);
+        else setPlayer2Time((prevTime) => prevTime - 100);
+      }, 48);
     }
 
     return () => clearInterval(interval);
@@ -27,24 +28,28 @@ const ChessClock = () => {
   const switchPlayer = () => setActivePlayer((prevPlayer) => (prevPlayer === 1 ? 2 : 1));
 
   const resetClock = () => {
-    setPlayer1Time(300);
-    setPlayer2Time(300);
+    setPlayer1Time(initialTimeInSeconds * 1000);
+    setPlayer2Time(initialTimeInSeconds * 1000);
     setActivePlayer(0);
   };
 
   const formatTime = (time) => {
-    const minutes = Math.floor(time / 60);
-    const seconds = time % 60;
-    return `${String(minutes).padStart(2, '0')}:${String(seconds).padStart(2, '0')}`;
+    const minutes = Math.floor(time / 60000);
+    const seconds = Math.floor((time % 60000) / 1000);
+    if (time > 0) {
+      return `${String(minutes).padStart(2, '0')}:${String(seconds).padStart(2, '0')}`;
+    } else {
+      return "Venceu";
+    }
   };
 
   return (
     <MainView>
       {activePlayer === 0 ? (
-        <StartButton onPress={() => startClock(1)} ><TextBtn>Start Clock</TextBtn></StartButton>
+        <StartButton onPress={() => startClock(1)}><TextBtn>Start Clock</TextBtn></StartButton>
       ) : (
         <>
-          <PlayerButton onPress={switchPlayer} disabled={activePlayer == 1 ? true : false}>
+          <PlayerButton onPress={switchPlayer} disabled={activePlayer === 1}>
             <TextPlayerBtn>{formatTime(player2Time)}</TextPlayerBtn>
           </PlayerButton>
           <InternalGameOptions>
@@ -52,7 +57,7 @@ const ChessClock = () => {
               <TextOptionsButton onPress={resetClock}>Reset</TextOptionsButton>
             </TouchableOpacity>
           </InternalGameOptions>
-          <PlayerButton onPress={switchPlayer} disabled={activePlayer == 2 ? true : false}>
+          <PlayerButton onPress={switchPlayer} disabled={activePlayer === 2}>
             <TextPlayerBtn>{formatTime(player1Time)}</TextPlayerBtn>
           </PlayerButton>
         </>
